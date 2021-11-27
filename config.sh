7e0a741c8cfe1528992a508d6f07a2bc9db68153
Bedirhan KURT <windowz414@gnuweeb.org>
2021-11-27 11:50:43 +0300

    Add plenty of features
    * Introducing manifest-less device source gathering! The old school
      git clone way, implemented the config way! If you don't know how
      to make a manifest, simply comment "local_manifest_url" out in
      config.sh and edit the last 4 variables accordingly. Comment out
      the non-existent ones, add as you need. This will also require
      relevant changes on sync.sh too. I'll try to make this more
      unattended in later commits.
    
    * Introducing messages with images embedded for Telegram chat
      announcements! telegram.sh version 0.3 introduced support for
      text stylings for all message types.
      Commit: https://github.com/fabianonline/telegram.sh/commit/707335ac53778903cf030a152ff640a81815749d
      (set parse mode for all types of messages)
    
      Added this feature as I tested that it won't cause any problems
      with messages sent.
      Tested-chat: https://t.me/owZ_Builds/343
      Note: The first 2 builds testing this were unable to locate build
      fail image. (build.sh was sourcing config.sh again, which resets
      RELEASES_DIR variable. Hardcoded redeclaration of RELEASES_DIR
      correctly by echoing output of `cd -` for now. Future versions
      of bash/zsh might break it. This behavior will change in a future
      commit.)
    
    Signed-off-by: Bedirhan KURT <windowz414@gnuweeb.org>

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export RELEASES_DIR=$(pwd)
+
 export GITHUB_USER="alexwcrafter"
 export GITHUB_EMAIL="kontakt@ayokaacr.de"
 
@@ -11,6 +13,7 @@ export official="false"
 export local_manifest_url="https://github.com/ACRBuilds/local_manifests/raw/a1f551f3c16c7609a7cbb22e863708a1607d6d20/line-sofiar.xml"
 export manifest_url="https://github.com/LineageOS/android"
 export rom_vendor_name="lineage"
+#export referencedir="ROMNAME"
 export branch="lineage-19.0"
 export bacon="bacon"
 export buildtype="userdebug"
@@ -26,3 +29,9 @@ export jenkins="true"
 export release_repo="ACRBuilds/releases"
 
 export timezone="UTC"
+
+# You'll need to configure these accordingly. Also edit sync.sh accordingly too.
+export devicetree="https://github.com/teos-dev/android_device_vestel_teos-lin15" devicepath="device/vestel/teos" devicebranch="lineage-15.1"
+export devicetreecommon="https://github.com/teos-dev/android_device_vestel_msm8920-common" devicepathcommon="device/vestel/msm8920-common" devicebranchcommon="lineage-15.1"
+export kerneltree="https://github.com/teos-dev/android_kernel_vestel_msm8920" kernelpath="kernel/vestel/msm8920" kernelbranch="lineage-15.1"
+export vendortree="https://github.com/teos-dev/android_vendor_vestel-lin15" vendorpath="vendor/vestel" vendorbranch="lineage-15.1"
