eca4c90f4f2029c9ddbee0e32dbe8fb16b0d7b6e
Beru Hinode <windowz414@1337.lgbt>
2022-08-26 08:15:51 +0000

    Do more refreshes
    * Remove all associations with Git-Polycule for now
    
    * Set git config only when necessary, don't do global configuration
      if possible.
    
    * `source build/envsetup.sh` before doing clean and clobber, these
      have been discontinued.
    
    * Some more little adjustments.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- dirty.sh
+++ dirty.sh
@@ -20,25 +20,6 @@ if [ -z "${GITHUB_TOKEN}" ]; then
     fi
 fi
 
-GITLAB_HOST="git.polycule.co"
-
-if [ -z "${POLYAMOROUS_TOKEN}" ]; then
-    if [ -z "${GITLAB_TOKEN}" ]; then
-        if [ -z "${GLAB_TOKEN}" ]; then
-            echo "Please set POLYAMOROUS_TOKEN, GITLAB_TOKEN or GLAB_TOKEN before continuing."
-            exit 1
-        else
-            export POLYAMOROUS_TOKEN="${GLAB_TOKEN}"
-        fi
-    else
-        export POLYAMOROUS_TOKEN="${GITLAB_TOKEN}"
-    fi
-fi
-
-# Email for git
-git config --global user.email "${GITHUB_EMAIL}"
-git config --global user.name "${GITHUB_USER}"
-
 cd "${ROM_DIR}"
 
 source "${my_dir}/sync.sh"
